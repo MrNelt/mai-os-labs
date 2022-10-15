bb1a5485ea697681f3696c90f5390aa5012ba3a8
kappaprideonly <kolmanoney@gmail.com>
2022-10-15 12:38:06 +0300

    fix

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -1,5 +1,6 @@
 #include "parent.h"
 
+
 int main() {
     ParentRoutine(std::cin, getenv("PATH_TO_CHILD"));
     return 0;
--- lab2/src/string_to_vector.cpp
+++ lab2/src/string_to_vector.cpp
@@ -3,9 +3,9 @@
 
 std::vector<float> StringToVectorFloats(std::string const& stringNumbers, char separator) {
     std::vector<float> results;
-    std::string::const_iterator start = stringNumbers.begin();
-    std::string::const_iterator end = stringNumbers.end();
-    std::string::const_iterator next = std::find(start, end, separator);
+    auto start = stringNumbers.begin();
+    auto end = stringNumbers.end();
+    auto next = std::find(start, end, separator);
     while (next != end) {
         results.push_back(stof(std::string(start, next)));
         start = next + 1;
